94b33dfdd0aacf17302552b0155d74cff5aa6dc1
Rahat Saini <Rahat.Saini@Finity.local>
2019-01-08 15:35:49 +1100

    initial comit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const hbs= require('hbs');
+var app = express();
+
+
+
+// for registring the partials
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+var fs = require('fs');
+
+    app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log =  `${now}: ${req.method}${req.url}`;
+    fs.appendFile('server.log',log + '\n');
+    console.log(`${now}: ${req.method}${req.url}`);
+    next();
+    })
+
+
+
+    app.use((req,res,next)=>{
+         res.render('maintaince.hbs');
+        });
+
+
+app.use(express.static(__dirname+'/public'));
+hbs.registerHelper('currentYear',()=>
+{return new Date().getFullYear();
+
+});
+
+
+
+
+
+app.get('/',(req,res)=>{ 
+    res.render('welcome.hbs',
+    {
+        pagetitle:'Welcome',
+        
+    });
+});
+
+
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',
+    {
+    pagetitle:'sometitle',
+    currentyear : new Date().getFullYear()
+    }
+    );
+});
+
+app.listen(3000);
