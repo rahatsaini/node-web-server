4ec826a370a033f2e833806e830f43132ba4311c
Rahat Saini <Rahat.Saini@Finity.local>
2019-01-09 16:30:36 +1100

    added project page

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/bad', (req, res) => {
   });
 });
 
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page',
+    
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
